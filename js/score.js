b4f9ff4b8d1eebb8345ee06298942e1967ba0db9
dao <tranvietdao1508@gmail.com>
2026-02-03 21:25:19 +0700

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,6 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    return Math.max(round(score), 0);
     let score = (-22.8*Math.pow(rank-1, 0.4) + 250) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
         score = Math.max(0, score);
