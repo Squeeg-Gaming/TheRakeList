19656256b6c522aeb78a5a89ff36db7a6bb388ee
dao <tranvietdao1508@gmail.com>
2026-03-25 21:38:52 +0700

    Update util.js

--- js/util.js
+++ js/util.js
@@ -186,3 +186,13 @@ export function shuffle(array) {
 
     return array;
 }
+export function getThumbnailImage(lvlName) {
+    return `../assets/levels/${encodeURIComponent(lvlName)}.png`;
+}
+export function embed(video) {
+    	if(video.includes("medal.tv")) {
+            return video;
+        } else {
+        	return `https://www.youtube.com/embed/${getYoutubeIdFromUrl(video)}?rel=0`;
+        }
+}
