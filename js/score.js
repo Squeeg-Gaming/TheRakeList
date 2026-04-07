5c2afbd63d8fa80e1f05c6e4f9302b8f53c2322b
sandwich <unspecifiedsandwich@gmail.com>
2026-04-07 15:44:38 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-28.8*Math.pow(rank-1, 0.4) + 10000) *
+    let score = (-28.8*Math.pow(rank-1, 0.4) + 1000000) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
